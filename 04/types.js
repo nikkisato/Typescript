c7cdc0dfae8bb7e172343f08a807a4db0422fab8
Nikki Sato <nikki.satopdx@gmail.comgit config --global user.email nikki.satopdx@gmail.com>
2021-05-26 20:09:22 -0700

    finished Brad Traversy video

---
+++ 04/types.js
@@ -0,0 +1,24 @@
+var myString;
+myString = 'Hello'.slice(0, 3);
+var myNum;
+myNum = 1;
+var myBool;
+myBool = false;
+var myVar;
+myVar = true;
+/*
+let strArr: string[];
+let numArr: number[];
+let boolArr: boolean[]
+*/
+var strArr;
+strArr = ['Hello', 'World'];
+var numArr;
+numArr = [1, 2, 3];
+var boolArr;
+boolArr = [true, false, true];
+var strNumTuple;
+strNumTuple = ['Hello', 4];
+var myVoid = null;
+var myUndefined = undefined;
+var myNull = null;
---
+++ 04/types.ts
@@ -0,0 +1,33 @@
+let myString: string;
+myString = 'Hello'.slice(0, 3);
+
+let myNum: number;
+myNum = 1;
+
+let myBool: boolean;
+myBool = false;
+
+let myVar: any;
+myVar = true;
+
+/*
+let strArr: string[];
+let numArr: number[];
+let boolArr: boolean[]
+*/
+
+let strArr: Array<string>;
+strArr = ['Hello', 'World'];
+
+let numArr: Array<number>;
+numArr = [1, 2, 3];
+
+let boolArr: Array<boolean>;
+boolArr = [true, false, true];
+
+let strNumTuple: [string, number];
+strNumTuple = ['Hello', 4];
+
+let myVoid: void = null;
+let myUndefined: undefined = undefined;
+let myNull: null = null;
